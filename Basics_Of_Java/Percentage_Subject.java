4f0a8f4fbaec1e16ebda297bad4f105b28d0b3dd
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-15 22:27:40 +0530

    Add files via upload

---
+++ Basics_Of_Java/Percentage_Subject.java
@@ -0,0 +1,12 @@
+public class Percentage_Subject {
+    public static void main(String[] args) {
+         //Code for calculating percentage
+         double x1 = 100; //Maths Marks
+         double x2 = 92;  //Chem Marks
+         double x3 = 90;  //English Marks
+         double x4 = 86;  //Economics Marks
+         double x5 = 77;  //Physics Marks
+         double p = (x1 + x2 + x3 + x4 + x5)/5;
+         System.out.println(p);
+    }
+}
