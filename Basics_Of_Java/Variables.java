6f18999980f7501010aea252a9d40721548f4e72
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-15 22:28:40 +0530

    Add files via upload

---
+++ Basics_Of_Java/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        x = 500;
+        System.out.println(x);
+        x = 400;
+        System.out.println(x);
+
+        x = x+5;
+        System.out.println(x);
+        
+        y = x-10;
+        System.out.println(y);
+
+    }
+}
