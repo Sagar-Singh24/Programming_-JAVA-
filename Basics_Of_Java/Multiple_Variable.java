9edc9305af2021ad875136cc043651076db00ff1
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-15 22:27:15 +0530

    Add files via upload

---
+++ Basics_Of_Java/Multiple_Variable.java
@@ -0,0 +1,30 @@
+public class Multiple_Variable {
+    public static void main(String[] args) {
+        int x=5,y=2,z=8;
+        System.out.print("The Value of X is: ");
+        System.out.println(x);
+        System.out.print("The value of Y is: ");
+        System.out.println(y);
+        System.out.print("The value of Z is: ");
+        System.out.println(z);
+
+        z = x;
+        System.out.println(z);
+
+
+        //Putting text and value together....This one is perfect writing of code
+
+        System.out.println("The Value of X is: "+x);
+        System.out.println("The Value of Y is: "+y);
+        System.out.println("The Value of Z is: "+z);
+
+        System.out.println(x+y+z);
+        System.out.println("Kuch Hatt kr millega:"+x+y+z);
+        System.out.println(+x+y+""+z);
+
+        System.out.println("Sagar"+"Singh");
+
+
+
+    }
+}
