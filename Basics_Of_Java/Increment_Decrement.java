e3bbb8fe4b57e7d014a4c2563e2523a81f8ddc95
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-15 22:26:36 +0530

    Add files via upload

---
+++ Basics_Of_Java/Increment_Decrement.java
@@ -0,0 +1,23 @@
+public class Increment_Decrement {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        x++;  //It means x = x+1 : Incrementy
+        System.out.println("Increment:"+x);
+
+        System.out.println(++x);
+        System.out.println(x);
+
+        //x++ is "Use X and then increment"[Post Increment]
+        //++x is "Use increment then use"[Pre Increment]
+
+//************************************************************************************ */
+        int y = 10;
+        System.out.println(y);
+        y--;  //It means y = y-1 : Decrement
+        System.out.println("Decrement:"+y);
+
+        
+
+    }
+}
