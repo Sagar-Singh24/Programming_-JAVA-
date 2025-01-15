5071f24d44e5fd8896975ed721d88ffa2039a6f8
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-15 22:25:50 +0530

    Add files via upload

---
+++ Basics_Of_Java/Arithmetic_Operations.java
@@ -0,0 +1,11 @@
+public class Arithmetic_Operations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
